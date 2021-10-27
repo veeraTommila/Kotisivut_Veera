8328132a8235b0b51b8ea362420452d51bf4b792
veeraTommila <78955844+veeraTommila@users.noreply.github.com>
2021-10-27 09:11:33 +0300

    I made a C#-file called HeiMaailma.cs and added a string variable to line 14 and a print command on line 15.
    I made a C#-file called HeiMaailma.cs and added a string variable to line 14 and a print command on line 15.

---
+++ HeiMaailma.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeiMaailma
+{
+	
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			String heiMaailma = "Hei maailma!";
+			Console.WriteLine(heiMaailma);
+		}
+	}
+	
+}
